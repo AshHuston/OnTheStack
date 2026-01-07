326e78e10ea9f307478cb92aea2b0012fbd8823a
AshHuston <dsguyhuston@gmail.com>
2026-01-07 16:07:04 -0500

    render port stuff 4

--- onthestack/src/backend/server.js
+++ onthestack/src/backend/server.js
@@ -2,16 +2,18 @@ import { startChronJobs } from "./chronjobs.js";
 import { ensureCurrentDatePuzzleInStore } from "./generatePuzzle.js";
 import express from 'express'
 import path from 'path'
+import { fileURLToPath } from "url";
 
 
 // ************ RENDER STUFF
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express()
 const PORT = process.env.PORT || 3000
-app.use(express.static('dist'))
+app.use(express.static(path.resolve(__dirname, '../dist')))
 // Serve API routes or static files here
 app.get('*', (req, res) => {
-    res.sendFile(path.resolve('dist/index.html'))
-  //res.send('Hello from Render!')
+    res.sendFile(path.resolve(__dirname, '../dist/index.html'))
 })
 
 // Render provides the port via env variable
