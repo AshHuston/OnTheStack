dd89414ca4159e4b0e9971de370dbbcefeddcb1c
AshHuston <dsguyhuston@gmail.com>
2026-01-07 13:49:27 -0500

    render port stuff

--- onthestack/src/backend/server.js
+++ onthestack/src/backend/server.js
@@ -1,6 +1,25 @@
 import { startChronJobs } from "./chronjobs.js";
 import { ensureCurrentDatePuzzleInStore } from "./generatePuzzle.js";
+import express from 'express'
 
-// Startup tasks
+
+// ************ RENDER STUFF
+const app = express()
+
+// Serve API routes or static files here
+app.get('/', (req, res) => {
+  res.send('Hello from Render!')
+})
+
+// Render provides the port via env variable
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`)
+})
+
+
+
+
+// ******** Startup tasks
 startChronJobs()
 ensureCurrentDatePuzzleInStore()
