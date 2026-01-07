fb1fc3a23e75caa08385413780d6865c799e0da5
AshHuston <dsguyhuston@gmail.com>
2026-01-07 18:50:43 -0500

    render port stuff 8

--- onthestack/src/backend/server.js
+++ onthestack/src/backend/server.js
@@ -30,6 +30,7 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const distPath = path.resolve(__dirname, '../dist')
+console.log(distPath)
 
 const app = express()
 const PORT = process.env.PORT || 3000
