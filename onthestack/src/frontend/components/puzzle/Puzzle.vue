210845c0bddf428d47ce2a10d566c48904f281e4
AshHuston <dsguyhuston@gmail.com>
2026-01-08 14:13:51 -0500

    render port stuff 9

--- onthestack/src/frontend/components/puzzle/Puzzle.vue
+++ onthestack/src/frontend/components/puzzle/Puzzle.vue
@@ -1,11 +1,27 @@
 <script setup>
+import CardName from './CardName.vue'
+import cardGuessField from './CardGuessField.vue'
 
+const guess = ref('')
 </script>
 
 
 <template>
     <div style="border: 2px solid red">TEST PUZZLE COMPIENNT</div>
-  
+  <cardGuessField v-model:guess="guess" showCardSuggestions="false" />
+
+  <CardName
+        :cardData="{
+            isFirstWord: false,
+            isLastWord: false,
+            topConnector: 'ger',
+            cardname: 'geralf, visionary stitcher',
+            bottomConnector: 'her',
+            blankMap: '______, _________ ________',
+            isSolved: false
+            }"
+        :isSolved="true"
+    />
 </template>
 
 <style>
