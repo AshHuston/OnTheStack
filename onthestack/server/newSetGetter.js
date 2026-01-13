c26b89916d34a8943ca3e6d83963f474d43f85ce
AshHuston <dsguyhuston@gmail.com>
2026-01-13 11:55:45 -0500

    cleanup adn setup for newset checker

---
+++ onthestack/server/newSetGetter.js
@@ -0,0 +1,15 @@
+export function ensureNewestSet() {
+    /* 
+    Get newest set that has had its prerelase date pass.(Release-1wk)
+        Check if it is aleady in our newSet.json --> Early return
+
+    Check set_type to ensure its acceptable.
+        iF NOT, EARLY RETRUN
+
+    Build cardpool with its cards.
+
+    Write it to the pool.
+
+    Maybe set flag for some kind of announcment callout or sumn?
+    */
+}
