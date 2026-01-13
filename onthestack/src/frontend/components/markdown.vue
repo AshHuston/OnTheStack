12b3151f04fe0e808fa4e92b08851952725cbf26
AshHuston <dsguyhuston@gmail.com>
2026-01-13 11:18:38 -0500

    reduced pool size and added date

---
+++ onthestack/src/frontend/components/markdown.vue
@@ -0,0 +1,18 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <div class="markdown" v-html="html"></div>
+</template>
+
+<script setup>
+import { computed } from 'vue'
+import { marked } from 'marked'
+
+const props = defineProps({
+  content: {
+    type: String,
+    required: true
+  }
+})
+
+const html = computed(() => marked.parse(props.content))
+</script>
---
+++ onthestack/src/frontend/components/puzzle/PuzzleDate.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { getFormattedDate } from '@/helpers';
+
+</script>
+
+<template>
+    <div>
+        {{ getFormattedDate() }}
+    </div>
+</template>
+
+<style>
+
+</style>
