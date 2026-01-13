caf62a7d4ed23a133dbb83d02a04d7fa9b04822c
AshHuston <dsguyhuston@gmail.com>
2026-01-12 19:54:40 -0500

    move settings to menu

---
+++ onthestack/src/frontend/components/settingsDialog.vue
@@ -0,0 +1,55 @@
+<script setup>
+import '@awesome.me/webawesome/dist/components/dialog/dialog.js';
+import '@awesome.me/webawesome/dist/components/checkbox/checkbox.js';
+import { useSettingsStore } from '@/stores/settings';
+
+const props = defineProps({
+  open: Boolean,
+})
+
+const settingsStore = useSettingsStore()
+
+const onChangeAutocomplete = (event) => {
+  settingsStore.autoComplete = event.target.checked
+}
+const onChangeHighlight = (event) => {
+  settingsStore.highlight = event.target.checked
+}
+const onChangeShowFirstLetter = (event) => {
+  settingsStore.showFirstLetter = event.target.checked
+}
+</script>
+
+<template>
+    <wa-dialog
+        :open="props.open" 
+        label="Settings"
+    >
+        <div class="wa-cluster">
+            <wa-checkbox 
+                size="small" 
+                :checked="settingsStore.autoComplete" 
+                @change="onChangeAutocomplete"
+            >Autocomplete</wa-checkbox>
+            <wa-checkbox 
+                size="small"
+                :defaultChecked="settingsStore.highlight" 
+                :checked="settingsStore.highlight" 
+                @change="onChangeHighlight"
+            >Highlight</wa-checkbox>
+            <wa-checkbox 
+                size="small"
+                :defaultChecked="settingsStore.showFirstLetter" 
+                :checked="settingsStore.showFirstLetter" 
+                @change="onChangeShowFirstLetter"
+            >First Letter</wa-checkbox>
+        </div>
+        <template v-slot:footer>
+            <wa-button  variant="brand" data-dialog="close">Close</wa-button>
+        </template>
+    </wa-dialog>
+</template>
+
+<style>
+
+</style>
